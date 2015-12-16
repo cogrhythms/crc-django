f21f23eef91f2d1905e8268068c7859c8d78cd45
Michael Romano <michaelfromano@gmail.com>
2015-12-16 10:31:10 -0500

    added models to models.py

--- crc/models.py
+++ crc/models.py
@@ -1,5 +1,27 @@
 from django.contrib.auth.models import User
 from django.db import models
+from crc.widgets import *
 
-class orig_model(models.Model):
-    pass
+class People(models.Model):
+    name = models.CharField(max_length=50, blank=False)
+    position = models.CharField(max_length=20, choices=POSITION_CHOICES, default='')
+    title = models.CharField(max_length=20, choices=TITLE_CHOICES, default='AF')
+    affiliation = models.CharField(max_length=50, blank=False)
+    topics = models.ManyToManyField('Topics')
+    groups = models.ManyToManyField('Groups')
+
+class Topics(models.Model):
+    name = models.CharField(max_length=50, blank=False)
+    description = models.TextField()
+
+class Papers(models.Model):
+    citation = models.TextField()
+
+class Courses(models.Model):
+    title = models.CharField(max_length=100, blank=False)
+    description = models.TextField()
+    professor = models.ForeignKey('People')
+
+class Groups(model.Model):
+    name = models.CharField(max_length=100, blank=False)
+    description = models.TextField()
